80a74fd4bb87395f2c43dbbfd62ae65c05e5f70d
ThrishikaUdayakumar <142384932+ThrishikaUdayakumar@users.noreply.github.com>
2023-08-18 12:59:21 +0530

    editing it with getelementsbyclassname and then by getelementbytagname

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ change[2].style.backgroundColor='green';
 for(let i=0;i<change.length;i++){
     change[i].style.fontWeight='bold';
 }
+var list=document.getElementsByTagName('li');
+list[4].style.backgroundColor='yellow';
+var classname=document.getElementsByClassName('list-group-item');
+for(let i=0;i<classname.length;i++){
+    classname[i].style.backgroundColor=('violet');
+}
