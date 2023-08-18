4a54c6ffbaed190ae70a3fa10932682f607dad32
ThrishikaUdayakumar <142384932+ThrishikaUdayakumar@users.noreply.github.com>
2023-08-18 12:37:46 +0530

    list font changed and background color for list 2 changed

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ for (let i = 0; i < elements.length; i++) {
     elements[i].style.fontWeight = "bold";
     elements[i].style.color="green";
 }
+var change=document.getElementsByClassName('list-group-item');
+change[2].style.backgroundColor='green';
+for(let i=0;i<change.length;i++){
+    change[i].style.fontWeight='bold';
+}
