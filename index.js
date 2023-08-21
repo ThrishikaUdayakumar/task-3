688788b182f3311cb24f308edac5d491b78a1b91
ThrishikaUdayakumar <142384932+ThrishikaUdayakumar@users.noreply.github.com>
2023-08-21 15:14:17 +0530

    edit button added

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function submitoperation(e) {
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(`${name.value} ${email.value} ${phone.value}`));
 
+    var editButton = document.createElement('button');
+    editButton.className = 'btn btn-primary btn-sm float-right js-button edit';
+    editButton.textContent = 'edit';
+    li.appendChild(editButton);
+    
     var newButton = document.createElement('button');
     newButton.className = 'btn btn-primary btn-sm float-right js-button delete';
     newButton.textContent = 'delete';
@@ -21,11 +26,21 @@ function submitoperation(e) {
     name.value = '';
     email.value = '';
     phone.value = '';
+    
 }
 
 // Use event delegation for the delete function
 userList.addEventListener('click', function(e) {
-    if (e.target.classList.contains('delete')) {
+    if (e.target.classList.contains('edit')) {
+        var li = e.target.parentElement;
+        var textContent = li.firstChild.textContent.trim();
+        var values = textContent.split(' ');
+
+        name.value = values[0];
+        email.value = values[1];
+        phone.value = values[2];
+        userList.removeChild(li); // Remove the old entry for editing
+    } else if (e.target.classList.contains('delete')) {
         if (confirm('Sure?')) {
             var li = e.target.parentElement;
             userList.removeChild(li);
